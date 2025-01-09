3d85fa839136c04434e569212ecfb525c52a34fd
zoiebackman <zoiebackman@icloud.com>
2025-01-09 21:31:53 +0100

    sum sidan klar

--- common.js
+++ common.js
@@ -1,31 +1,38 @@
 
 let createButton = document.querySelector(".createButton");
 let createNumbersInput = document.querySelector(".createNumbers");
-
 let numbersBox = document.querySelector(".numbersBox");
+
+let numbersArray = [];
+console.log(numbersArray);
 
 createNumbersInput.value = 95;
 
 
 function createNumbersBox(value) {
     let number = parseInt(createNumbersInput.value);
-
+    numbersArray = [];
     numbersBox.innerHTML = "";
 
     for (let i = 1; i <= number; i++) {
         let randomValue = Math.floor(Math.random() * 100);
+        numbersArray.push(randomValue);
         let cell = document.createElement("div");
         cell.classList.add("allNumbersBoxCell");
         cell.textContent = randomValue;
         numbersBox.appendChild(cell);
     }
+    console.log(numbersArray)
+    return numbersArray
 }
 
 createButton.addEventListener("click", function () {
     createNumbersBox(createNumbersInput.value)
 })
 
+
 createNumbersBox(createNumbersInput.value);
+
 
 
 
--- sum/sum.js
+++ sum/sum.js
@@ -1,20 +1,57 @@
 
-function sumOfNumber(numberArray) {
+function sumOfNumber(array) {
     let sum = 0
 
-    for (let i = 0; i < numberArray.length; i++) {
-        sum += numberArray[i];
+    for (let i = 0; i < array.length; i++) {
+        sum += array[i];
     }
 
     return sum;
 }
 
 function sumOfMarkedNumber() {
+    let sum = 0;
+    let markedCells = document.querySelectorAll(".allNumbersBoxCell.greenBox")
 
+    for (let cell of markedCells) {
+        sum += parseInt(cell.textContent);
+    }
+
+    return sum;
 }
 
 let sumOfAll = document.getElementById("sumNumberBox");
 let sumOfMarked = document.getElementById("sumOfMarkedBox");
+let resetButton = document.getElementById("sumResetButton")
+
+sumOfAll.textContent = sumOfNumber(numbersArray);
+
+let cells = document.querySelectorAll(".allNumbersBoxCell");
+for (let cell of cells) {
+    cell.addEventListener("click", function () {
+        cell.classList.add("greenBox");
+        sumOfMarked.textContent = sumOfMarkedNumber();
+    });
+}
+
+createButton.addEventListener("click", function () {
+    sumOfAll.textContent = sumOfNumber(numbersArray);
+
+    let cells = document.querySelectorAll(".allNumbersBoxCell");
+    for (let cell of cells) {
+        cell.addEventListener("click", function () {
+            cell.classList.add("greenBox");
+            sumOfMarked.textContent = sumOfMarkedNumber();
+        });
+    }
+})
+
+resetButton.addEventListener("click", function () {
+    let cells = document.querySelectorAll(".allNumbersBoxCell");
+    for (let cell of cells) {
+        cell.classList.remove("greenBox");
+        sumOfMarked.textContent = "-"
+    }
+})
 
 
-sumOfAll.textContent = sumOfNumber();
