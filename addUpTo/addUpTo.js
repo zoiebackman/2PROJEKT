b3e6df20bdf3348a1fa3f2863dd19f0ac4dc47f2
zoiebackman <zoiebackman@icloud.com>
2025-01-12 16:57:05 +0100

    .

--- addUpTo/addUpTo.js
+++ addUpTo/addUpTo.js
@@ -2,3 +2,4 @@ let input = document.getElementById("addUpToInput");
 let twoCellsButton = document.getElementById("twoCells");
 let cells = document.querySelectorAll(".allNumbersBoxCell");
 
+input.value = 190;
