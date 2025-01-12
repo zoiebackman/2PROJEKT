18fc060a0d4523e210c40bb4ae12b49665296370
zoiebackman <zoiebackman@icloud.com>
2025-01-12 17:40:57 +0100

    klar med addUpTo

--- addUpTo/addUpTo.js
+++ addUpTo/addUpTo.js
@@ -2,4 +2,20 @@ let input = document.getElementById("addUpToInput");
 let twoCellsButton = document.getElementById("twoCells");
 let cells = document.querySelectorAll(".allNumbersBoxCell");
 
-input.value = 190;
+input.value = 179;
+
+twoCellsButton.addEventListener("click", function () {
+    let inputNumber = parseInt(input.value);
+
+    for (let i = 0; i < cells.length; i++) {
+        let firstNumber = parseInt(cells[i].textContent);
+        for (let j = i + 1; j < cells.length; j++) {
+            let secondNumber = parseInt(cells[j].textContent);
+            if (firstNumber + secondNumber == inputNumber) {
+                cells[i].classList.add("greenBox");
+                cells[j].classList.add("greenBox");
+                return;
+            }
+        }
+    }
+})
