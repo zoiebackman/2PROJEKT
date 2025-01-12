7ba4784b231232d0c60375fe2613895090a69be7
zoiebackman <zoiebackman@icloud.com>
2025-01-12 19:21:02 +0100

    tror jag  är klar

--- findSame/findSame.js
+++ findSame/findSame.js
@@ -1,10 +1,10 @@
-
 let cells = document.querySelectorAll(".allNumbersBoxCell");
 let findSameText = document.getElementById("findSameText");
 let resetButton = document.getElementById("resetButton");
 
 for (let cell of cells) {
     cell.addEventListener("click", function () {
+        cells = document.querySelectorAll(".allNumbersBoxCell")
         let counter = 0;
 
         for (let secondCell of cells) {
@@ -21,6 +21,7 @@ for (let cell of cells) {
     })
 
     resetButton.addEventListener("click", function () {
+        cells = document.querySelectorAll(".allNumbersBoxCell")
         findSameText.textContent = "Click on a number to find copies"
 
         for (let cell of cells) {
@@ -29,6 +30,37 @@ for (let cell of cells) {
     })
 }
 
+createButton.addEventListener("click", function () {
+    createNumbersBox(createNumbersInput.value)
+    cells = document.querySelectorAll(".allNumbersBoxCell");
+    for (let cell of cells) {
+        cell.addEventListener("click", function () {
+            cells = document.querySelectorAll(".allNumbersBoxCell")
+            let counter = 0;
+
+            for (let secondCell of cells) {
+                if (cell.textContent === secondCell.textContent) {
+                    secondCell.classList.add("greenBox");
+                    counter += 1;
+                } else {
+                    secondCell.classList.remove("greenBox")
+                    findSameText.textContent = "";
+                }
+
+                findSameText.textContent = `${counter} copies of the number ${cell.textContent}`;
+            }
+        })
+
+        resetButton.addEventListener("click", function () {
+            cells = document.querySelectorAll(".allNumbersBoxCell")
+            findSameText.textContent = "Click on a number to find copies"
+
+            for (let cell of cells) {
+                cell.classList.remove("greenBox");
+            }
+        })
+    }
+})
 
 
 
